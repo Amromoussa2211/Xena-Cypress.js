48e89a402b5d2d83c81cc691c3a988f070e6c16c
Amr <46786529+Amromoussa2211@users.noreply.github.com>
2024-08-25 12:07:48 +0300

    Update index.js

--- support/index.js
+++ support/index.js
@@ -1,7 +1,17 @@
 // cypress/support/index.js
-
-// Import commands.js using ES2015 syntax:
-import './commands';
-
 // Alternatively you can use CommonJS syntax:
 // require('./commands');
+// cypress/support/index.js
+
+// Import custom commands for use in tests
+import './commands';
+
+// Set up plugins and other utilities used across tests
+import 'cypress-localstorage-commands'; 
+require('cypress-xpath');
+import '@shelex/cypress-allure-plugin';
+
+// Set up a global event handler for uncaught exceptions
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false; // Prevent Cypress from failing the test
+});
